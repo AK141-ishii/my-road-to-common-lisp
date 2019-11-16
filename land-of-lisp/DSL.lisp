fdbf7c654ab08d5706ca63134e07d6a5628624ed
aknorsh <A.I141@outlook.jp>
2019-11-16 18:16:59 +0900

    Add DSL.lisp

---
+++ land-of-lisp/DSL.lisp
@@ -0,0 +1,31 @@
+; ########################################
+; DSL : Domain Specific Language
+; ########################################
+
+; SVG format : file format for drawing graphics
+; based on Arc by Paul Graham
+
+; printing <tag att...> or </tag>
+(defun print-tag (name alst closingp)
+  (princ #\<)
+  (when closingp
+    (princ #\/))
+  (princ (string-downcase name))
+  (mapc (lambda (att)
+          (format t " ~a=\"~a\"" (string-downcase (car att)) (cdr att)))
+        alst)
+  (princ #\>))
+;test (print-tag 'my-tag '((color . blue) (height . 9)) nil)
+
+(defmacro tag (name atts &body body)
+  `(progn (print-tag ',name
+                     (list ,@(mapcar (lambda (x)
+                                       `(cons ',(car x) ,(cdr x)))
+                                     (pairs atts)))
+                     nil)
+          ,@body
+          (print-tag ',name nil t)))
+
+
+
+

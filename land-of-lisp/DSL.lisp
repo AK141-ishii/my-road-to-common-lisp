6ce2a1754f15403ce47a595a9a465814b780bd53
aknorsh <A.I141@outlook.jp>
2019-11-17 04:45:15 +0900

    fin dice-of-doom graphics

--- land-of-lisp/DSL.lisp
+++ land-of-lisp/DSL.lisp
@@ -103,14 +103,14 @@
                            (1+ value))
                        (1- len)))))
 
-(with-open-file (*standard-output* "test_random_walk.svg"
-                                   :direction :output
-                                   :if-exists :supersede)
-  (svg 400 200 (loop repeat 10
-                     do (polygon (append '((0 . 200))
-                                         (loop for x
-                                               for y in (random-walk 100 400)
-                                               collect (cons x y))
-                                         '((400 . 200)))
-                                 (loop repeat 3
-                                       collect (random 256))))))
+;(with-open-file (*standard-output* "test_random_walk.svg"
+;                                   :direction :output
+;                                   :if-exists :supersede)
+;  (svg 400 200 (loop repeat 10
+;                     do (polygon (append '((0 . 200))
+;                                         (loop for x
+;                                               for y in (random-walk 100 400)
+;                                               collect (cons x y))
+;                                         '((400 . 200)))
+;                                 (loop repeat 3
+;                                       collect (random 256))))))

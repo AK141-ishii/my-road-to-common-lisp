cc6f84df0bd81e97e58a9758d302c0d939be0869
AKINORI Ishii <akinori.ishii@toyokumo.co.jp>
2019-12-26 17:31:17 +0900

    proceed

--- macro-training.lisp
+++ macro-training.lisp
@@ -9,6 +9,21 @@
 
 ; #########################
 
+; Text ###################
+
+(defmacro our-dolist ((var list &optional result) &body body)
+  `(progn
+     (mapc #'(lambda (,var) ,@body)
+           ,list)
+     (let ((,var nil))
+       ,result)))
+(defmacro when-bind ((var expr) &body body)
+  `(let ((,var ,expr))
+     (when ,var
+       ,@body)))
+
+; #########################
+
 ; Prac
 
 ; (memq x choices)
@@ -27,5 +42,42 @@
        ((not ,rule))
        ,@body))
 
+; (do ((w 3)
+;      (x 1 (1+ x))
+;      (y 2 (1+ y))
+;      (z))
+;   ((> x 10) (princ z) y)
+;   (princ x)
+;   (princ y))
+; -> 
+; (prog ((w 3) (x 1) (y 2) (z nil))
+;       foo
+;       (if (> x 10)
+;           (return (progn (princ z) y)))
+;       (princ x)
+;       (princ y)
+;       (psetq x (1+ x) y (1+ y))
+;       (go foo))
 
+; TODO
+;(defmacro our-do (bindforms (test &rest res) &body body)
+;  `(prog ,()
+;     
+;     )
+;  
+;  )
+
+(defun initbind (bindforms)
+  (mapcar (lambda (a)
+            (if (null (cdr a))
+                (list (car a) nil)
+                (list (car a) (cadr a))))
+          bindforms))
+
+; TODO
+;(defun stepforms (bindforms)
+;  (mapcan (lambda (a)
+;            (if ())
+;            ))
+;  )
 
